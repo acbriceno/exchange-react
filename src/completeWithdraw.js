96169e840ab44ee7ebc48a9add4f796828107b5a
acbriceno <acbriceno@live.com>
2021-04-19 07:51:57 -0230

    complete button but not fully functional

---
+++ src/completeWithdraw.js
@@ -0,0 +1,92 @@
+import React, {useRef, useState} from "react";
+import auth from "./auth";
+import roleManager from './RoleManager'
+import Toast from 'react-bootstrap/Toast'
+
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import {gql, useMutation} from '@apollo/client'
+
+const  completeWithdrawalM = gql `
+    mutation completeDeposit($transactionID: String!){
+      completeDeposit(transactionID: $transactionID)
+    }
+  `
+
+ var initialState = {
+   transactionID: ''
+ }
+
+ const resetState = {
+   transactionID: ''
+ }
+
+
+
+export const CompleteDepositPage = props => {
+// if(auth.getRole() != null){
+  // props.history.push(roleManager.getStartingRoute(auth.getRole())) 
+// }
+  const [state, setState] = useState(initialState)
+  const [errors, setErrors] = useState('')
+   const [show, setShow] = useState(false)
+
+	console.log(props)
+	
+  const [completeDeposit, { loading, error, data }] = useMutation(completeDepositM, {
+    onCompleted: (data) =>{
+      if(data !== undefined ){
+       console.log(data)
+        if(data.completeDeposit === null){
+        }else{
+            if(!data.completeDeposit){
+            return (<p> Deposit not Completed </p>)
+          }
+          initialState = resetState
+          setState(initialState)
+          setShow(true)
+        }
+
+      }
+
+    },
+    onError: (error) => console.error("Error", error)
+  })
+
+  if(loading){return (<p> Loading... </p>)}
+  
+  async function completeDepositHandler() {
+    //event.preventDefault();
+    completeDeposit({ variables: {transactionID: props.transactionID} });
+  }
+  console.log("?????")
+
+
+  const handleInput = e => {
+    const inputName = e.currentTarget.name
+    const value = e.currentTarget.value
+    setState(prev => ({...prev, [inputName]: value}))
+  }
+ 
+  return (
+    <>
+    <div className="row">
+    <Button size="sm" onClick={e => completeDepositHandler()}>Confirm Complete Deposit</Button>
+    <Toast onClose={() => setShow(false)} show={show} delay={2300} autohide>
+          <Toast.Header>
+            <img
+              src="holder.js/20x20?text=%20"
+              className="rounded mr-2"
+              alt=""
+            />
+            <strong className="mr-auto">Currency Management</strong>
+            <small>Just now</small>
+          </Toast.Header>
+          <Toast.Body>Deposit Completed</Toast.Body>
+        </Toast>
+
+    </div>
+    </>
+  );
+};
+
--- src/transactions.js
+++ src/transactions.js
@@ -2,9 +2,9 @@ import React, {useRef, useEffect, useState} from "react";
 import auth from "./auth";
 import roleManager from './RoleManager'
 import ListGroup from 'react-bootstrap/ListGroup'
-import Button from 'react-bootstrap/Button'
+import {Button, Modal} from 'react-bootstrap'
 import {gql, useQuery} from '@apollo/client'
-
+import {CompleteDepositPage} from './completeDeposit'
 const getTransactions = gql`
 {
   getTransactions{
@@ -33,11 +33,34 @@ const initialState = {
 
 }
 
+function CompleteDeposit(props) {
+  return (
+    <Modal
+      {...props}
+    size="sm"
+      centered
+    >
+       <Modal.Header closeButton>
+        <Modal.Title className="text-center">
+          Deposit
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body className="text-center">
+            <CompleteDepositPage  transactionID={props.transactionID} />
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
 export const TransactionsPage = props => {
 
   const {loading: transactionLoading, error:transactionError, data:transactionData} = useQuery(getTransactions)
  const {loading:currencyLoading, error:currencyError, data:currencyData} = useQuery(getCurrencies)
  const [state, setState] = useState(initialState)
+ const [modalShow, setModalShow] = useState(false);
   useEffect(() => {
     setState(state => {
       if(!currencyLoading && !transactionLoading){
@@ -68,8 +91,21 @@ export const TransactionsPage = props => {
       return id
   }
   
+   const completeDepositHandler = e =>{
+   		e.preventDefault()
+   		console.log("inside cdh")
+   		
+   		
+  
+  }
+  
  const TransactionsList = state.transactions.map((transaction) =>
 <React.Fragment key={transaction.id}>
+<CompleteDeposit 
+		transactionID={transaction.id}
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
    <ListGroup.Item key={transaction.id}> 
 
   <div className="row">
@@ -79,8 +115,12 @@ export const TransactionsPage = props => {
      <p>  {"Transaction Type: " + transaction.type} {"   "} {"Status: " + transaction.status} </p>
     </div>
     <div className="col-sm-6" >
-        <button  className="btn btn-danger btn-sm btn-space">Cancel</button>
-        <button  className="btn btn-success btn-sm">Complete</button>
+        <button  className="btn btn-danger btn-sm btn-space" value={transaction.id}
+        onClick={e => completeDepositHandler(e)}
+        >Cancel</button>
+        <button  className="btn btn-success btn-sm" value={transaction.id}
+           onClick={e => setModalShow(true)}
+        >Complete</button>
     </div>
   </div>
   </ListGroup.Item>
@@ -92,6 +132,7 @@ export const TransactionsPage = props => {
   
   return (
     <>
+    
       <ListGroup>{TransactionsList}</ListGroup>
     </>
   );
